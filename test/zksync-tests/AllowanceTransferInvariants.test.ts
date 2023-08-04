136001df9bed234cc04ea1ca52e07039dcc746b3
dmitriyischenko <124681031+DmitriyIschenko@users.noreply.github.com>
2023-08-05 02:03:33 +0300

    pr-review-changes

--- test/zksync-tests/AllowanceTransferInvariants.test.ts
+++ test/zksync-tests/AllowanceTransferInvariants.test.ts
@@ -4,14 +4,13 @@ import {deployContract, provider} from "./shared/zkSyncUtils";
 import fs from "fs";
 import {BigNumber, BigNumberish, ethers} from "ethers";
 import {expect} from "./shared/expect";
-import {getCompactPermitSignature, PermitSingle} from "./utils/PermitSignature";
+import {buildPermitSingle, getCompactPermitSignature, PermitSingle} from "./utils/PermitSignature";
 
 const RICH_WALLET_PRIVATE_KEYS = JSON.parse(fs.readFileSync("test/zksync-tests/shared/rich-wallets.json", 'utf8'));
 const DECIMAL_MULT: BigNumber = ethers.BigNumber.from(10).pow(ethers.BigNumber.from(18));
 const defaultExpiration: BigNumber = ethers.BigNumber.from(Date.now() + 50000);
 
 describe("AllowanceTransferInvariants", function () {
-
     let permit2: Permit2;
     let token: MockERC20;
     let spender1: Wallet = new Wallet(RICH_WALLET_PRIVATE_KEYS[4].privateKey, provider);
@@ -33,7 +32,6 @@ describe("AllowanceTransferInvariants", function () {
         await (await token.connect(permitter1).approve(permit2.address, ethers.constants.MaxUint256)).wait();
         await (await token.connect(permitter2).approve(permit2.address, ethers.constants.MaxUint256)).wait();
 
-
         chosePermitter = (Math.floor(Math.random() * 2)) ? permitter1 : permitter2;
         choseSpender = (Math.floor(Math.random() * 2)) ? spender1 : spender2;
     });
@@ -42,16 +40,7 @@ describe("AllowanceTransferInvariants", function () {
         it('spent should not exceeds permitted', async function () {
             let permitted: BigNumber = DECIMAL_MULT;
 
-            let permit: PermitSingle = {
-                details: {
-                    token: token.address,
-                    amount: permitted,
-                    expiration: defaultExpiration,
-                    nonce: defaultNonce,
-                },
-                spender: choseSpender.address,
-                sigDeadline: 2000000000,
-            };
+            let permit: PermitSingle = buildPermitSingle(token.address, permitted, defaultExpiration, defaultNonce, choseSpender.address, 2000000000);
 
             let startBalanceFrom: BigNumberish = await token.connect(chosePermitter).balanceOf(chosePermitter.address);
 
@@ -70,17 +59,7 @@ describe("AllowanceTransferInvariants", function () {
     describe("Balance Equals Spent", function () {
         it('balance should equals spent', async function () {
             let permitted: BigNumber = DECIMAL_MULT;
-
-            let permit: PermitSingle = {
-                details: {
-                    token: token.address,
-                    amount: permitted,
-                    expiration: defaultExpiration,
-                    nonce: defaultNonce,
-                },
-                spender: choseSpender.address,
-                sigDeadline: 2000000000,
-            };
+            let permit: PermitSingle = buildPermitSingle(token.address, permitted, defaultExpiration, defaultNonce, choseSpender.address, 2000000000)
 
             let startBalanceFrom: BigNumberish = await token.connect(chosePermitter).balanceOf(chosePermitter.address);
 
@@ -100,6 +79,4 @@ describe("AllowanceTransferInvariants", function () {
             expect(await token.connect(spender1).balanceOf(permit2.address)).to.be.equal(ethers.constants.Zero);
         });
     });
-
-
 });
